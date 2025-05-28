77098375318a6f5ed2d71e2eb1b2ac5d646e78a1
Daniel Nurlanbekov <daniel.nurlanbekov@gmail.com>
2025-05-28 17:38:02 +0600

    Пятый коммит

--- banana.py
+++ banana.py
@@ -3,7 +3,13 @@
 #
 # say_hello()
 
-def greet(name):
-    print(f"Привет, {name}!")
+# def greet(name):
+#     print(f"Привет, {name}!")
+#
+# greet("Алиса")
 
-greet("Алиса")
+def square(number):
+    return number ** 2
+
+result = square(5)
+print(result)
